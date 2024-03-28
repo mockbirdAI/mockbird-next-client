a7520b9052eceec945577e1266cc2db8d8027708
Ricky Liao <liaoric@uw.edu>
2024-03-27 21:43:33 -0700

    change to us-east-1

--- src/app/api/accept-interview/route.ts
+++ src/app/api/accept-interview/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: any) {
       Accept: 'application/json',
       Authorization: `Basic ${dyteAuth}`
     },
-    body: '{"title":"string","preferred_region":"ap-south-1","record_on_start":false,"live_stream_on_start":false,"recording_config":{"max_seconds":60,"file_name_prefix":"string","video_config":{"codec":"H264","width":1280,"height":720,"watermark":{"url":"http://example.com","size":{"width":1,"height":1},"position":"left top"},"export_file":true},"audio_config":{"codec":"AAC","channel":"stereo","export_file":true},"storage_config":{"type":"aws","access_key":"string","secret":"string","bucket":"string","region":"us-east-1","path":"string","auth_method":"KEY","username":"string","password":"string","host":"string","port":0,"private_key":"string"},"dyte_bucket_config":{"enabled":true},"live_streaming_config":{"rtmp_url":"rtmp://a.rtmp.youtube.com/live2"}}}'
+    body: '{"title":"string","preferred_region":"us-east-1","record_on_start":false,"live_stream_on_start":false,"recording_config":{"max_seconds":60,"file_name_prefix":"string","video_config":{"codec":"H264","width":1280,"height":720,"watermark":{"url":"http://example.com","size":{"width":1,"height":1},"position":"left top"},"export_file":true},"audio_config":{"codec":"AAC","channel":"stereo","export_file":true},"storage_config":{"type":"aws","access_key":"string","secret":"string","bucket":"string","region":"us-east-1","path":"string","auth_method":"KEY","username":"string","password":"string","host":"string","port":0,"private_key":"string"},"dyte_bucket_config":{"enabled":true},"live_streaming_config":{"rtmp_url":"rtmp://a.rtmp.youtube.com/live2"}}}'
   };
   
   try {
