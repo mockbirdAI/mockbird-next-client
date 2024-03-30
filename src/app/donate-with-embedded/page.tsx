fe543349c125075c0ba49c6afee42c6e0153da9e
Ricky Liao <liaoric@uw.edu>
2024-03-29 18:17:19 -0700

    stripe functionality

---
+++ src/app/donate-with-embedded/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+import CheckoutForm from "@/common/components/form/CheckoutForm";
+
+export const metadata: Metadata = {
+  title: "Donate with embedded Checkout | Next.js + TypeScript Example",
+};
+
+export default function DonatePage(): JSX.Element {
+  return (
+    <div className="page-container">
+      <h1>Donate with embedded Checkout</h1>
+      <p>Donate to our project 💖</p>
+      <CheckoutForm uiMode="embedded" />
+    </div>
+  );
+}
---
+++ src/lib/stripe.ts
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: "2023-10-16",
+  appInfo: {
+    name: "StealthXI",
+    url: "https://stealthxi.com",
+  },
+});
