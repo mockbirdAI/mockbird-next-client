553bd9906e374ba22b39d651d68a0a18b538f000
Ricky Liao <liaoric@uw.edu>
2024-04-04 00:39:45 -0700

    onboarding fix

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // Add your prop types here
+}
+
+const Onboarding: React.FC<Props> = () => {
+  return (
+    <div>
+      <p>onboarding</p>
+    </div>
+  );
+};
+
+export default Onboarding;
