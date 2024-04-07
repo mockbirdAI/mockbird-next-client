08f300c7b06f70358537483ddfe6c1072b8cd6e1
Ricky Liao <liaoric@uw.edu>
2024-04-07 15:53:56 -0700

    middleware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,4 +4,4 @@ import { authOptions } from "./lib/auth";
 
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ['/dashboard/:path*'] }
+export const config = { matcher: ['/dashboard/:path*', '/onboarding'] }
