2e87524326ba470c923a67c6dc28c8d6498fc245
Ricky Liao <liaoric@uw.edu>
2024-04-15 16:34:44 -0700

    clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import Loading from "./loading";
 import { redirect } from "next/navigation";
 import ProdNavbar from "@/common/components/ProdNavbar";
 import Script from "next/script";
+import MicrosoftClarity from "@/app/metrics/MicrosoftClarity";
 
 const inter = Inter({ subsets: ["latin"] });
 const lato = Lato({ subsets: ["latin"], weight: "400" });
@@ -27,17 +28,6 @@ export default function RootLayout({
   
   return (
     <html lang="en">
-
-      <script type="text/javascript">
-        {`
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "lx54jv1870");
-        `}
-      </script>
-
       <body>
         <Provider>
           <main className={lato.className}>
@@ -47,6 +37,7 @@ export default function RootLayout({
             
               <Suspense fallback={<Loading />}>
                 {children}
+                <MicrosoftClarity />
               </Suspense>
             <Footer />
             <Toaster />
