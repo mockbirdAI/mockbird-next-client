4fcb60af8199d0075e59048d9cab64916319866b
Ricky Liao <liaoric@uw.edu>
2024-04-15 17:22:34 -0700

    stripe.ts change

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,7 +4,7 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: "2023-10-16",
   appInfo: {
-    name: "StealthXI",
-    url: "https://stealthxi.com",
+    name: "Mockbird",
+    url: "https://www.mockbird.ai",
   },
 });
