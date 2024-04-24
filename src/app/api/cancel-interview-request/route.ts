f08e49c45b5369fa41d0f4df1fbd1e19247c0244
Ricky Liao <liaoric@uw.edu>
2024-04-23 20:14:45 -0700

    cancel paymentintent

--- src/app/api/cancel-interview-request/route.ts
+++ src/app/api/cancel-interview-request/route.ts
@@ -1,19 +1,44 @@
 import prisma from "@/lib/prisma";
 import { RequestStatus } from "@prisma/client";
 import { NextResponse } from "next/server"
+import Stripe from "stripe";
 
 export async function POST(request: any) {
   const res = await request.json()
   const { requestId } = res;
 
-   const updateRequest = await prisma.interviewRequest.update({
-    where: {
-      id: requestId,
-    },
-    data: {
-      status: RequestStatus.CANCELLED,
-    }
-   })
+   try {
+    const interviewReq = await prisma.interviewRequest.findUniqueOrThrow({
+      where: {
+        id: requestId,
+      },
+      select: {
+        paymentId: true
+      }
+    });
 
-  return NextResponse.json({ updateRequest })
+    if (interviewReq.paymentId) {
+      const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY));
+      try {
+        const paymentInt = await stripe.paymentIntents.cancel(interviewReq.paymentId);
+      } catch (err) {
+        console.error(err);
+        throw err;
+      }
+    }
+
+    const updateRequest = await prisma.interviewRequest.update({
+      where: {
+        id: requestId,
+      },
+      data: {
+        status: RequestStatus.CANCELLED,
+      }
+     })
+
+    return NextResponse.json({ updateRequest })
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
