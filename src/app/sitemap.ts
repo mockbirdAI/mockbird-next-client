2e652be0d50ade205ea6a53ba9a31735f2be9d2e
Ricky Liao <liaoric@uw.edu>
2024-04-27 22:39:52 -0700

    sitemap modifications

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,24 +5,24 @@ import { UserRole } from '@prisma/client';
 type changeFrequency = 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  
-  let recruiters = await prisma.user.findMany({
-    where: {
-      role: UserRole.RECRUITER
-    }
-  })
+  // let recruiters = await prisma.user.findMany({
+  //   where: {
+  //     role: UserRole.RECRUITER
+  //   }
+  // })
 
-  const recruiterRoutes = recruiters.map((recruiter) => ({
-    url: `https://mockbird.ai/recruiter/${recruiter.id}`,
-    lastModified: new Date(),
-    changeFrequency: 'daily' as changeFrequency,
-  }))
+  // const recruiterRoutes = recruiters.map((recruiter) => ({
+  //   url: `https://mockbird.ai/recruiter/${recruiter.id}`,
+  //   lastModified: new Date(),
+  //   changeFrequency: 'daily' as changeFrequency,
+  // }))
 
-  const routes = ['', '/dashboard'].map((route) => ({
+  const routes = [''].map((route) => ({
     url: `https://mockbird.ai${route}`,
     lastModified: new Date(),
     changeFrequency: 'daily' as changeFrequency,
   }))
   
-  return [...routes, ...recruiterRoutes]
+  // return [...routes, ...recruiterRoutes]
+  return [...routes]
 }
