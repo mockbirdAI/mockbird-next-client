2b8f1b2e949f0f1fa14e4af27c37962d9de9d00b
Ricky Liao <liaoric@uw.edu>
2024-05-21 19:09:58 -0700

    sort by order

--- src/app/api/get-company-data/route.ts
+++ src/app/api/get-company-data/route.ts
@@ -2,6 +2,10 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const res = await prisma.company.findMany();
+  const res = await prisma.company.findMany({
+    orderBy: {
+      name: 'asc'
+    }
+  });
   return NextResponse.json({ res })
 }
--- src/app/api/get-school-data/route.ts
+++ src/app/api/get-school-data/route.ts
@@ -2,6 +2,10 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const res = await prisma.school.findMany();
+  const res = await prisma.school.findMany({
+    orderBy: {
+      name: 'asc'
+    }
+  });
   return NextResponse.json({ res })
 }
